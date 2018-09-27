cac20a657252d5244d6eeca65616e9802947e358
Ariel Zhen <zhenpingping@live.com>
2018-09-27 22:00:27 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,21 @@
 class Song < ActiveRecord::Base
   # add associations here
   belongs_to :artist
-  belongs_to :genre 
+  belongs_to :genre
+
+  def artist_name=(name)
+
+  end
+
+  def artist_name
+
+  end
+
+  def genre_name=(name)
+  end
+
+  def genre_name
+
+  end
+  
 end
